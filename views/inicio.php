b5ad19ae19a822ed33c3ef6dc38c3455c634e7a0
jafajardo2001 <jafajardo@est.istg.edu.ec>
2024-02-18 22:05:31 -0500

    cambio de formato a php

---
+++ views/inicio.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inicio</title>
+    <link rel="shortcut icon" href="assets/brand/notes-logo-ico.svg">
+
+    <link rel="canonical" href="https://getbootstrap.com/docs/5.3/examples/sign-in/">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@docsearch/css@3">
+
+    <link href="assets/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="assets/dist/css/sign-in.css" rel="stylesheet">
+</head>
+
+<body>
+
+</body>
+
+</html>
